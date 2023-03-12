ae93c1b75b39e3a4226b31fa2450942feec58b97
ismailulgac-23 <82471789+ismailulgac-23@users.noreply.github.com>
2023-03-12 14:37:41 +0300

    Update Modal.vue

--- src/components/shared/Modal.vue
+++ src/components/shared/Modal.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-screen h-full overflow-hidden fixed top-0 left-0">
-    <div class="w-full h-full bg-black flex items-center justify-center">
+    <div class="w-full h-full bg-[#292830] flex items-center justify-center">
       <slot></slot>
     </div>
   </div>
